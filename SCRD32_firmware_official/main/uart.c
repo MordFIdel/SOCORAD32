d200b6912f6e63b4b47789b0cf7edc4d6f279287
joel <joel.martin.2104@gmail.com>
2023-08-25 14:29:41 +0200

    Add option to disable ct dcs tone squelch with 0

--- SCRD32_firmware_official/main/uart.c
+++ SCRD32_firmware_official/main/uart.c
@@ -93,19 +93,21 @@ void uartChannelSetting()
 
     if(uartTxBuf[30] == 'R' && uartTxBuf[31] == 'C')
     {
+    /*
         if(gRxCtcss == 0) gRxCtcss = 1;
         else if(gRxCtcss > 38) gRxCtcss = 38;
 
         if(gTxCtcss == 0) gTxCtcss = 1;
         else if(gTxCtcss > 38) gTxCtcss = 38;
+    */
          
-        uartTxBuf[30] = gCtcssList[gRxCtcss-1][0];
-        uartTxBuf[31] = gCtcssList[gRxCtcss-1][1];
+        uartTxBuf[30] = gCtcssList[gRxCtcss][0];
+        uartTxBuf[31] = gCtcssList[gRxCtcss][1];
     }
     if(uartTxBuf[33] == 'T' && uartTxBuf[34] == 'C')
     {
-        uartTxBuf[33] = gCtcssList[gTxCtcss-1][0];
-        uartTxBuf[34] = gCtcssList[gTxCtcss-1][1];
+        uartTxBuf[33] = gCtcssList[gTxCtcss][0];
+        uartTxBuf[34] = gCtcssList[gTxCtcss][1];
     }
     if(uartTxBuf[36] == 'B')
     {
